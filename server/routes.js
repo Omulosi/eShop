d8f67aac6472621db1612494bcb2748f0730b49d
Omulosi <mulongojohnpaul@gmail.com>
2021-07-22 15:12:38 +0300

    Add product list and detail routes. Set up stripe integration with node

---
+++ server/routes.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const products = require("./products.json");
+
+module.exports = function getRoutes() {
+  const router = express.Router();
+
+  router.get("/products", getProducts);
+  router.get("/products/:productId", getProduct);
+
+  return router;
+};
+
+const getProducts = (req, res) => {
+  res.status(200).json({ products });
+};
+
+const getProduct = (req, res) => {
+  const { productId } = req.params;
+  const product = products.find((product) => product.id === productId);
+
+  try {
+    if (!product) {
+      throw Error(`No product found for id: ${productId}`);
+    }
+    res.status(200).json({ product });
+  } catch (error) {
+    return res.status(404).json({ statusCode: 404, message: error.message });
+  }
+};
