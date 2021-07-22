b4414722b6ca3ea016a5152aad374b9499731680
Omulosi <mulongojohnpaul@gmail.com>
2021-07-22 16:16:00 +0300

    Add modal for managing cart items

--- client/src/components/CartSummary.js
+++ client/src/components/CartSummary.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { useShoppingCart } from "use-shopping-cart";
+import CartModal from "./CartModal";
 import { CartIcon } from "./Icons";
 
 export default function CartSummary() {
   const { cartCount, formattedTotalPrice } = useShoppingCart();
+
+  const [isOpen, setOpen] = React.useState(false);
+
+  const toggleModal = () => setOpen(!isOpen);
   return (
     <>
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <nav
+        className="md:ml-auto flex flex-wrap items-center text-base justify-center"
+        onClick={toggleModal}
+      >
         <span className="mr-5 hover:text-white flex items-center">
           <CartIcon />
           <span className="ml-3">
@@ -14,6 +22,7 @@ export default function CartSummary() {
           </span>
         </span>
       </nav>
+      <CartModal isOpen={isOpen} toggleModal={toggleModal} />
     </>
   );
 }
